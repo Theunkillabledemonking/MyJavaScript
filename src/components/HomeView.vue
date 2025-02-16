7dc16b8ba90c2a21cdb2080f1d8732458f586ae6
Theunkillabledemonking <166793267+Theunkillabledemonking@users.noreply.github.com>
2025-02-16 14:32:10 +0900

    Create HomeView.vue

---
+++ src/components/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>홈 페이지</h1>
+    <ButtonComponent label="클릭하세요" @clicked="handleClick" />
+  </div>
+</template>
+
+<script>
+  import ButtonComponent from "../components/ButtonComponent.vue";
+
+  export default {
+    components: { ButtonComponent },
+    methods: {
+      handleClick() {
+        alert("홈 페이지 버튼 클릭!");
+      }
+    }
+  };
+</script>
