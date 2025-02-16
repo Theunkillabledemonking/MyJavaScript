2cbd9341ae4fdd94c9a891a98f88078c67f944ed
Theunkillabledemonking <166793267+Theunkillabledemonking@users.noreply.github.com>
2025-02-16 15:00:51 +0900

    게시글 id에 따라 동적으로 데이터를 불러오는 코드

---
+++ src/components/PostView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div> 
+    <h1>{{ post.title }}</h1>
+    <p>{ post.content }</p>
+  </div>
+</template>
+
+<script>
+  import { usePostStore } from "../store/posts";
+  import { useRoute } from "vue-router";
+
+  export default {
+    setup() {
+      const postSotre = usePostStore();
+      const route = useRoute();
+      const post = postStore.getPost(route.parmas.id);
+      return { post };
+    }
+  };
+</script>
