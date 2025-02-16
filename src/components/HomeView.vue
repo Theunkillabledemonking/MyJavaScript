179b14ca8cb963c4785420a336f7debb02a23bfc
Theunkillabledemonking <166793267+Theunkillabledemonking@users.noreply.github.com>
2025-02-16 15:32:02 +0900

    Update HomeView.vue
    메인페이지 구동.

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,19 +1,35 @@
 <template>
   <div>
+    <Header />
     <h1>홈 페이지</h1>
-    <ButtonComponent label="클릭하세요" @clicked="handleClick" />
+
+    <section>
+      <h2>공지사항</h2>
+      <ul>
+        <li v-for="(notice, index) in notices" :key="index">{{ notice }}</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2>시간표</h2>
+      <ul>
+        <li v-for="(schedule, index) in schedules" :key="index">{{ schedule }}</li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
-  import ButtonComponent from "../components/ButtonComponent.vue";
+  import Header from "../components/Header.vue";
+  import { ref } from "vue";
 
   export default {
-    components: { ButtonComponent },
-    methods: {
-      handleClick() {
-        alert("홈 페이지 버튼 클릭!");
-      }
+    components: { Header },
+    setup() {
+      const notices = ref(["학사 일정 공지", "수업 변경 안내", "시험 일정 발표"]);
+      const schedules = ref(["월요일: 수학", "화요일: 일본어", "수요일: 프로그래밍"]);
+
+      return { notices, schedules };
     }
   };
 </script>
